fa4ef55633ff50de712824dc5b45787d29245080
Nancy <n007ancyjaiswal@gmail.com>
2026-01-21 14:13:11 +0530

    Time: 11 ms (3.15%), Space: 44.2 MB (6.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Arrays.sort(nums);
+        for(int num:nums){
+            System.out.println(num);
+        }
+        
+    }
+}
