42e008c38e9ddf2265d094ec88e5fff7c7385bef
Nancy <n007ancyjaiswal@gmail.com>
2026-03-16 19:39:11 +0530

    Time: 3 ms (80.83%), Space: 43.4 MB (77.33%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+            if (op.equals("+")) {
+                int last = stack.pop();
+                int sum = last + stack.peek();
+                stack.push(last);
+                stack.push(sum);
+            } else if (op.equals("D")) {
+                stack.push(stack.peek() * 2);
+            } else if (op.equals("C")) {
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int total = 0;
+        for (int score : stack) {
+            total += score;
+        }
+        return total;
+    }
+}
